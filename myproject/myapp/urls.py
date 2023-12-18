e4a7f0135c53bb5ef889c5acff0bacd1775e1b8b
siddu pagadala <siddupagadala880@gmail.com>
2023-12-18 18:35:52 +0530

    project commit

---
+++ myproject/myapp/urls.py
@@ -0,0 +1,42 @@
+"""
+URL configuration for myproject project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',views.index,name='index'),
+    path('Registration/', views.Registration,name='Registration'),
+    path('emp_login/', views.emp_login,name='emp_login'),
+    path('emp_home/', views.emp_home,name='emp_home'),
+    path('profile/', views.profile,name='profile'),
+    path('logout/', views.Logout,name='logout'),
+    path('admin_login/', views.admin_login,name='admin_login'),
+    path('my_experience/', views.my_experience,name='my_experience'),
+    path('edit_myexperience/', views.edit_myexperience,name='edit_myexperience'),
+    path('my_education/', views.my_education,name='my_education'),
+    path('edit_myeducation/', views.edit_myeducation,name='edit_myeducation'),
+    path('change_password/', views.change_password,name='change_password'),
+    path('admin_home/', views.admin_home,name='admin_home'),
+    path('change_passwordadmin/', views.change_passwordadmin,name='change_passwordadmin'),
+    path('all_employee/', views.all_employee,name='all_employee'),
+
+
+
+
+]
